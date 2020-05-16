ff2553cfacb083479f005891d745bf791cca6d3d
Tony Clulow HP <tony.clulow@pentadtech.com>
2020-05-16 21:42:30 +0100

    Shield buttons.

---
+++ Panel/Buttons.h
@@ -0,0 +1,51 @@
+/** Buttons
+ */
+#ifndef _Buttons_h
+#define _Buttons_h
+
+
+// The analog button
+#define BUTTON_ANALOG 0
+
+
+// Button values.
+#define BUTTON_NONE   0
+#define BUTTON_RIGHT  1
+#define BUTTON_UP     2
+#define BUTTON_DOWN   3
+#define BUTTON_LEFT   4
+#define BUTTON_SELECT 5
+
+
+/** Read the input button pressed.
+ *  Return one of the constants above.
+ */
+int readButton()
+{
+  int value = analogRead(BUTTON_ANALOG);
+  
+  if (value < 60) 
+  {
+    return BUTTON_RIGHT;
+  }
+  else if (value < 200)
+  {
+    return BUTTON_UP;
+  }
+  else if (value < 400)
+  {
+    return BUTTON_DOWN;
+  }
+  else if (value < 600)
+  {
+    return BUTTON_LEFT;
+  }
+  else if (value < 800)
+  {
+    return BUTTON_SELECT;
+  }
+
+  return BUTTON_NONE;
+}
+
+#endif
