8a85364b85184b72c4d1652cb8007cf51a7deef4
Tony Clulow <tony.clulow@pentadtech.com>
2020-06-08 23:05:40 +0100

    Extra debugger button options.

--- Panel/Debug.ino
+++ Panel/Debug.ino
@@ -7,6 +7,7 @@ boolean debugEnabled(int aLevel)
 
 
 /** Pause for user-input if so configured.
+ *  Use buttons to adjust debug level.
  */
 void debugPause()
 {
@@ -15,10 +16,19 @@ void debugPause()
     switch (waitForButton())
     {
       case BUTTON_NONE:   break;
-      case BUTTON_UP:     break;
-      case BUTTON_DOWN:   break;
-      case BUTTON_SELECT: break;
-      case BUTTON_LEFT:   systemData.debugLevel = DEBUG_OFF;
+      case BUTTON_UP:     systemData.debugLevel += 1;
+                          if (systemData.debugLevel >= DEBUG_MAX)
+                          {
+                            systemData.debugLevel = DEBUG_MAX - 1;
+                          }
+                          saveSystemData();
+                          break;
+      case BUTTON_DOWN:   systemData.debugLevel -= 1;
+                          saveSystemData();
+                          break;
+      case BUTTON_SELECT: configure.run();
+                          break;
+      case BUTTON_LEFT:   systemData.debugLevel = 0;
                           saveSystemData();
                           announce();
                           break;
