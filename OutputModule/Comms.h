82e5baed40ea5737b7f3ce3a2cc6e341642ae7e2
Tony Clulow HP <tony.clulow@pentadtech.com>
2020-12-10 10:52:43 +0000

    Re-order comms commands.

--- OutputModule/Comms.h
+++ OutputModule/Comms.h
@@ -10,12 +10,14 @@
 
 
 #define COMMS_CMD_STATES    0x00    // Read the state of all node's Outputs.
-#define COMMS_CMD_SET_LO    0x10    // Go Lo
-#define COMMS_CMD_SET_HI    0x20    // Go Hi
-#define COMMS_CMD_READ      0x40    // Read data from Output's definition (to the i2c master).    
-#define COMMS_CMD_WRITE     0x50    // Write data to Output's definition (from the i2c master).
-#define COMMS_CMD_SAVE      0x60    // Write data to Output's definition and save it.
-#define COMMS_CMD_RESET     0x70    // Reset output to its saved state.
+#define COMMS_CMD_DEBUG     0x10    // Set debug level.
+#define COMMS_CMD_SET_LO    0x20    // Go Lo
+#define COMMS_CMD_SET_HI    0x30    // Go Hi
+
+#define COMMS_CMD_READ      0x40    // Read data from Output's EEPROM definition (to the i2c master).    
+#define COMMS_CMD_WRITE     0x50    // Write data to Output's EEPROM definition (from the i2c master).
+#define COMMS_CMD_SAVE      0x60    // Write data to Output's EEPROM definition and save it.
+#define COMMS_CMD_RESET     0x70    // Reset output to its saved state (from its EEPROM).
 
 #define COMMS_CMD_NONE      0xff    // Null command.
 
