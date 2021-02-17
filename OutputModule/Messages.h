2a9fd74e563297f8f49d57b4e8ca83cd4756f033
Tony Clulow <tony.clulow@pentadtech.com>
2021-02-17 18:33:03 +0000

    Version 3.0.4

--- OutputModule/Messages.h
+++ OutputModule/Messages.h
@@ -23,7 +23,7 @@
 #else
     const char M_SOFTWARE[]     PROGMEM = "Output module";
 #endif
-const char M_VERSION[]          PROGMEM = "v3.0.3";        // See also system.VERSION.
+const char M_VERSION[]          PROGMEM = "v3.0.4";        // See also system.VERSION.
 const char M_VERSION_DATE[]     PROGMEM = "Feb 21";
 const char M_STARTUP[]          PROGMEM = "Startup";
 const char M_UPDATE[]           PROGMEM = "Update ";
