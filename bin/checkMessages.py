252113998fbfc299734a2b76dff53e149f9d6153
Tony Clulow <tony.clulow@pentadtech.com>
2021-04-25 23:33:41 +0100

    Python script to check for unused messages.

---
+++ bin/checkMessages.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# Check all M_ messages are used.
+
+import os
+import re
+
+namePattern = "^\s*const\s*char\s*(M_[A-Z]*)\[\]\s*PROGMEM"
+
+messageFileName = "Messages.h"
+fileNames = os.listdir()
+fileNames.remove(messageFileName)
+
+unused = 0
+
+# Process all lines in the messageFile
+with open(messageFileName) as messageFile:
+    for line in messageFile:
+        match = re.match(namePattern, line)
+
+        # Process all messages
+        if match is not None:
+            found = False
+            messageName = match.group(1)
+
+            # Check if messageFile itself references the message
+            with open(messageFileName) as messageFile2:
+                refPattern = "^.* " + messageName + "[ ,]"
+                for messageLine in messageFile2:
+                    matchSelf = re.match(refPattern, messageLine)
+                    if matchSelf is not None:
+                        found = True
+                        # print("Reference", messageName, "in", messageFileName)
+                        break
+
+            # Check if any other file references the message
+            for fileName in fileNames:
+                if found:
+                    # print("Found", messageName)
+                    break
+
+                with open(fileName) as sourceFile:
+                    for sourceLine in sourceFile:
+                        if messageName in sourceLine:
+                            found = True
+                            # print("Found", messageName, "in", fileName)
+                            break
+
+            if not found:
+                print("Unused", messageName)
+                unused += 1
+
+if not unused:
+    print("All messages used")
