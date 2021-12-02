7ee71ba20022b6547a5dab8e3a3f1993d699756c
Tony Clulow HP <tony.clulow@pentadtech.com>
2021-12-02 12:23:27 +0000

    Version 4.0.1

--- SignalBox/Messages.h
+++ SignalBox/Messages.h
@@ -24,8 +24,8 @@
 #elif SB_OUTPUT_MODULE
     const char M_SOFTWARE[]     PROGMEM = "Output module";
 #endif
-const char M_VERSION[]          PROGMEM = "v4.0.0";        // See also system.VERSION.
-const char M_VERSION_DATE[]     PROGMEM = "Nov 21";
+const char M_VERSION[]          PROGMEM = "v4.0.1";        // See also system.VERSION.
+const char M_VERSION_DATE[]     PROGMEM = "Dec 21";
 const char M_INIT_I2C[]         PROGMEM = "Init I2C";
 const char M_STARTUP[]          PROGMEM = "Startup";
 const char M_UPDATE[]           PROGMEM = "Update";
