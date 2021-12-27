f845996f372e33e97df23989b164761ed2d9a4ff
Tony Clulow DT <tony.clulow@pentadtech.com>
2021-12-27 13:11:47 +0000

    Remove redundant forward declarations from SignalBox.h

--- SignalBox/SignalBox.h
+++ SignalBox/SignalBox.h
@@ -25,46 +25,4 @@ uint16_t currentSwitchState[INPUT_NODE_MAX];    // Current state of inputs.
 boolean hasLcdShield = LCD_SHIELD;              // An LCD shield is present.
 
 
-/** Scan for attached hardware (Input and Output nodes).
- */
-void scanHardware();
-
-
-/** Read the pins of a InputNode.
- *  Return the state of the pins, 16 bits, both ports.
- *  Return current state if there's a communication error,
- *  this will prevent any actions being performed.
- */
-uint16_t readInputNode(uint8_t aNode);
-
-
-/** Scan all the Inputs.
- *  Parameter indicates if Configuration is in progress.
- */
-void scanInputs(boolean aConfiguration);
-
-
-/** Process all the Input's Outputs.
- */
-void processInputOutputs(boolean aNewState);
-
-
-/** Process an Input's n'th Output, setting it to the given state.
- *  Accumulate delay before or after movement depending on direction outputs are being processed.
- */
-uint8_t processInputOutput(uint8_t aIndex, uint8_t aState, uint8_t aDelay);
-
-
-/** Send a command to an output node.
- *  Return error code if any.
- *  Forward reference required for Configure class.
- */
-int sendOutputCommand(uint8_t aValue, uint8_t aPace, uint8_t aDelay, uint8_t aState);
-
-
-/** Sends the current debug level to all the connected outputs.
- */
-void sendDebugLevel();
-
-
 #endif
