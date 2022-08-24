e122163a4dbe56723cef74b77291c7d3931b112e
Tony Clulow DT <tony.clulow@pentadtech.com>
2022-08-24 18:08:14 +0100

    Version 4.4.1

--- OutputModule/Messages.h
+++ OutputModule/Messages.h
@@ -25,7 +25,7 @@
 #elif SB_OUTPUT_MODULE
     const char M_SOFTWARE[]     PROGMEM = "Output module";
 #endif
-const char M_VERSION[]          PROGMEM = "v4.4.0";        // See also SystemMgr.VERSION.
+const char M_VERSION[]          PROGMEM = "v4.4.1";        // See also SystemMgr.VERSION.
 const char M_VERSION_DATE[]     PROGMEM = "Aug 22";
 const char M_INIT_I2C[]         PROGMEM = "Init I2C";
 const char M_STARTUP[]          PROGMEM = "Startup";
