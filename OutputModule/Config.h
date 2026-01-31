2f789ce3a0cbe16a7eb85cd354ac8a99773db8da
Tony Clulow DT <tony.clulow@pentadtech.com>
2026-01-31 22:41:27 +0000

    InputModules use IC IDs from 0x30-0x37.

--- OutputModule/Config.h
+++ OutputModule/Config.h
@@ -28,6 +28,7 @@
 const uint8_t  I2C_CONTROLLER_ID       = 0x10;      // Controller ID.
 const uint8_t  I2C_GATEWAY_ID          = 0x00;      // Gateway ID. Set to zero to disable Gateway code.
 const uint8_t  I2C_INPUT_BASE_ID       = 0x20;      // Input nodes base ID.
+const uint8_t  I2C_INP_MOD_BASE_ID     = 0x30;      // Input Module nodes base ID.
 const uint8_t  I2C_OUTPUT_BASE_ID      = 0x50;      // Output nodes base ID.
 const uint8_t  I2C_MODULE_ID_JUMPERS   = 0xff;      // Use jumpers to decide module ID.
 
