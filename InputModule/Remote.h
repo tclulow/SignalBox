0f3b68c8f17d145606c9db8350da5acc3f0eed6c
Tony Clulow DT <tony.clulow@pentadtech.com>
2026-02-02 16:14:12 +0000

    InputModule code to program IR commands and recognise them.

--- InputModule/Remote.h
+++ InputModule/Remote.h
@@ -1,7 +1,7 @@
 /** OutputModule.
  *  @file
  *
- *  (c)Copyright Tony Clulow  2021-25   antony.clulow@gmail.com
+ *  (c)Copyright Tony Clulow  2021-26   antony.clulow@gmail.com
  *
  *  This work is licensed under the:
  *      Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
@@ -15,10 +15,14 @@
 #define Remote_h
 
 #include <IRremote.hpp>
+#include <EEPROM.h>
 
 
 static const uint8_t REMOTE_PIN    = 11;        // Remote control IR pin.
 static const uint8_t INPUT_PIN_MAX = 16;        // 16 inputs to the node. Must match InputDef.INPUT_PIN_MAX.
+static const long    DELAY_EXPIRE  = 5000;      // Give up waiting for proramming after this interval (msecs).
+static const long    DELAY_WAITING = 20;        // Fast flicker when waiting for programming.
+
 
 
 /** Handle IR commands from a remote controller.
@@ -28,8 +32,15 @@ class Remote: public Persisted
 {
     private:
 
-    uint16_t flags = 0xffff;
-    uint32_t irCodes[INPUT_PIN_MAX];
+    struct IrCode
+    {   
+        public:
+        uint16_t address = 0;
+        uint16_t command = 0;
+    };
+
+    uint16_t flags = 0xffff;                    // All 16 inputs are high by default.
+    IrCode irCodes[INPUT_PIN_MAX];              // The programmed IR codes.
     
 
     public:
@@ -48,22 +59,28 @@ class Remote: public Persisted
     {
         if (aFirstRun)
         {
-            // Load the saved IR codes.
-            EEPROM.get(getBase(), irCodes);
-        }
-        else
-        {
             // First run, initialise the IR codes.
             for (int i = 0; i < INPUT_PIN_MAX; i++)
             {
-                irCodes[i] = 0;
+                irCodes[i].address = 0;
+                irCodes[i].command = 0;
             }
             EEPROM.put(getBase(), irCodes);
         }
+        else
+        {
+            // Load the saved IR codes.
+            EEPROM.get(getBase(), irCodes);
+        }
+
+        // Configure the on-board LED pin for output
+        pinMode(LED_BUILTIN, OUTPUT);       
 
         // Initialise the Receiver.
         IrReceiver.begin(REMOTE_PIN, ENABLE_LED_FEEDBACK);
         IrReceiver.printActiveIRProtocols(&Serial);
+
+        programIrCodes();
     }
 
 
@@ -89,11 +106,19 @@ class Remote: public Persisted
                     Serial.println(tDecodedRawData);
                 }
 
-                if (IrReceiver.decodedIRData.protocol != UNKNOWN)
+                // if (IrReceiver.decodedIRData.protocol != UNKNOWN)
                 {
                     // The info output for a successful receive
                     IrReceiver.printIRResultShort(&Serial);
                     IrReceiver.printIRSendUsage(&Serial);
+                }
+            }
+            for (uint16_t pin = 0; pin < INPUT_PIN_MAX; pin++)
+            {
+                if (   (irCodes[pin].address == IrReceiver.lastDecodedAddress)
+                    && (irCodes[pin].command == IrReceiver.lastDecodedCommand))
+                {
+                    flags = ~(1 << pin);
                 }
             }
         
@@ -122,6 +147,52 @@ class Remote: public Persisted
     uint16_t getFlags()
     {
         return flags;
+    }
+
+
+    private:
+
+    /** Program IR codes */
+    void programIrCodes()
+    {
+        unsigned long expire = millis() + DELAY_EXPIRE;
+        uint8_t pin = 0;
+
+        flash(pin, DELAY_BLINK_LONG);
+
+        // Loop waiting for a programming event.
+        while (   (pin < INPUT_PIN_MAX)
+               && (expire < millis()))
+        {
+            flash(1, DELAY_WAITING);
+
+            if (IrReceiver.decode())
+            {
+                irCodes[pin].address = IrReceiver.lastDecodedAddress;
+                irCodes[pin].command = IrReceiver.lastDecodedCommand;
+                flash(++pin, DELAY_BLINK);
+                IrReceiver.resume();
+                expire = millis() + DELAY_EXPIRE;
+            }
+        }
+
+        // Update the IR codes if they've been changed.
+        if (pin > 0)
+        {
+            EEPROM.put(getBase(), irCodes);
+        }
+    }
+
+
+    void flash(uint8_t aFlashes, long aInterval)
+    {
+        while (aFlashes-- > 0)                      // n flashes.
+        {
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(aInterval);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(aInterval);
+        }
     }
 };
 
